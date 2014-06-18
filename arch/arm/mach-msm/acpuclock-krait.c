d43f56cf981632972182ee94e1a5c19349f88bcc
aicjofs@comcast.net <aicjofs@comcast.net>
2014-06-18 15:22:55 -0700

    msm: cpu: overclock: added low (162Mhz) and high (1782Mhz) cpu
    Conflicts:
    	arch/arm/mach-msm/Kconfig

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -49,6 +49,12 @@
 #define PRI_SRC_SEL_HFPLL	1
 #define PRI_SRC_SEL_HFPLL_DIV2	2
 
+#ifdef CONFIG_LOW_CPUCLOCKS
+#define FREQ_TABLE_SIZE 39
+#else
+#define FREQ_TABLE_SIZE 35
+#endif
+
 #if defined(CONFIG_MACH_APQ8064_GVAR_CMCC)
 int limit_cpufreq = 0;
 #endif
@@ -932,7 +938,7 @@ static void __init bus_init(const struct l2_level *l2_level)
 }
 
 #ifdef CONFIG_CPU_FREQ_MSM
-static struct cpufreq_frequency_table freq_table[NR_CPUS][35];
+static struct cpufreq_frequency_table freq_table[NR_CPUS][FREQ_TABLE_SIZE];
 
 static void __init cpufreq_table_init(void)
 {
